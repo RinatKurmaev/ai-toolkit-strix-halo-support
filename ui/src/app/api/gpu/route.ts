e1f1d520600d5f8111f58d0603bc461c53737763
Rinat Kurmaev <RinatKurmaev@users.noreply.github.com>
2026-04-08 00:06:46 +0200

    added lookup of the amd gpu devices

--- ui/src/app/api/gpu/route.ts
+++ ui/src/app/api/gpu/route.ts
@@ -3,6 +3,7 @@ import { exec, execSync } from 'child_process';
 import { promisify } from 'util';
 import { createRequire } from 'module';
 import os from 'os';
+import * as fs from 'fs';
 
 const execAsync = promisify(exec);
 
@@ -136,27 +137,35 @@ export async function GET() {
       });
     }
 
-    // Check if nvidia-smi is available
+    // Check if nvidia-smi or amd-smi is available
     const hasNvidiaSmi = await checkNvidiaSmi(isWindows);
-
-    if (!hasNvidiaSmi) {
+    const hasAmdSmi = await checkAMDSmi(isWindows);
+    if (!hasNvidiaSmi && !hasAmdSmi) {
       return NextResponse.json({
         hasNvidiaSmi: false,
         isMac: false,
         gpus: [],
-        error: 'nvidia-smi not found or not accessible',
+        error: 'nvidia-smi or amd-smi not found or not accessible',
       });
     }
 
     // Get GPU stats
-    const gpuStats = await getGpuStats(isWindows);
-
-    return NextResponse.json({
-      hasNvidiaSmi: true,
-      gpus: gpuStats,
-    });
+    if (hasNvidiaSmi) {
+      const gpuStats = await getGpuStats(isWindows);
+      return NextResponse.json({
+        hasNvidiaSmi: true,
+        gpus: gpuStats,
+      });
+    } 
+    if(hasAmdSmi) {
+      const gpuStats = await getAMDGpuStats(isWindows);
+      return NextResponse.json({
+        hasNvidiaSmi: true,
+        gpus: gpuStats,
+      });
+    };
   } catch (error) {
-    console.error('Error fetching NVIDIA GPU stats:', error);
+    console.error('Error fetching GPU stats:', error);
     return NextResponse.json(
       {
         hasNvidiaSmi: false,
@@ -179,6 +188,17 @@ async function checkNvidiaSmi(isWindows: boolean): Promise<boolean> {
     } else {
       // Linux/macOS check
       await execAsync('which nvidia-smi');
+    }
+    return true;
+  } catch (error) {
+    return false;
+  }
+}
+async function checkAMDSmi(isWindows: boolean): Promise<boolean> {
+  try {
+    if (!isWindows) {
+      // Linux/macOS check
+      await execAsync('which amd-smi');
     }
     return true;
   } catch (error) {
@@ -249,3 +269,127 @@ async function getGpuStats(isWindows: boolean) {
   return gpus;
 }
 
+async function getAMDGpuStats(isWindows: boolean) {
+  const command = 'amd-smi static --json && echo ";" && amd-smi metric --json';
+  const { stdout } = await execAsync(command, {
+    env: { ...process.env, CUDA_DEVICE_ORDER: 'PCI_BUS_ID' },
+  });
+  var data = stdout.split(';');
+
+  var sdata = {};
+  var mdata = {};
+  try {
+      sdata = JSON.parse(data[0]);
+      mdata = JSON.parse(data[1]);
+  } catch (error) {
+    return [];
+  }
+
+  var gpus = sdata["gpu_data"].map(d => {
+    const i = amdParseInt(d["gpu"]);
+    const gpu_data = mdata["gpu_data"][i];
+    const mem_total = amdParseFloat(gpu_data["mem_usage"]["total_vram"]["value"]);
+    const mem_used =  amdParseFloat(gpu_data["mem_usage"]["used_vram"]["value"]);
+    const mem_free =  amdParseFloat(gpu_data["mem_usage"]["free_visible_vram"]["value"]);
+    const mem_utilization = ((1.0 - (mem_total - mem_free)) / mem_total) * 100;
+
+    try {
+      var temperature = amdParseInt(gpu_data["temperature"]["hotspot"]["value"]);
+      mdata = JSON.parse(data[1]);
+  } catch (error) {
+    console.log(error);
+    temperature = NaN;
+  }
+  if (isNaN(temperature))
+  {
+      try {
+        //fall back to reading from /sys/class/ec_su_axb35/temp1/temp
+        // This function dependant on this module
+        // https://github.com/cmetz/ec-su_axb35-linux
+        // Works only on certain strix halo boards
+        var raw_data = fs.readFileSync('/sys/class/ec_su_axb35/temp1/temp', 'utf-8');
+      var temperature = amdParseInt(raw_data);
+
+        } catch (error) {
+         var temperature = NaN;
+      }
+  }
+    var speed = gpu_data["fan"]["usage"]["value"];
+    if (isNaN(speed))
+      {
+      try {
+        //console.log('fall back to reading from /sys/class/ec_su_axb35/fanX/rpm')
+        var raw_data = fs.readFileSync('/sys/class/ec_su_axb35/fan1/rpm', 'utf-8');
+        raw_data +=', ';
+        raw_data += fs.readFileSync('/sys/class/ec_su_axb35/fan2/rpm', 'utf-8');
+        raw_data +=', ';
+        raw_data += fs.readFileSync('/sys/class/ec_su_axb35/fan3/rpm', 'utf-8');
+        var speed = raw_data;
+        } catch (error) {
+         var speed = NaN;
+      }
+  }
+    try {
+        var gpu_usage = gpu_data["usage"]["gfx_activity"]["value"];
+    }  catch (error) {
+         var gpu_usage = NaN;
+      }
+
+    return {
+      index: i,
+      name: d["asic"]["market_name"],
+      driverVersion: d["driver"]["version"],
+      temperature: temperature,
+      utilization: {
+        gpu: gpu_usage,
+        memory: mem_utilization,
+      },
+      memory: {
+        total: mem_total,
+        used:  mem_used,
+        free:  mem_free,
+      },
+      power: {
+        draw: amdParseFloat(gpu_data["power"]["socket_power"]["value"]),
+        limit: amdParseFloat(() => {
+	  try {
+	    if (d["limit"]["max_power"]) {
+	      return d["limit"]["max_power"]["value"];
+	    } else if (d["limit"]["ppt0"]["max_power_limit"]["value"]) {
+	      return d["limit"]["ppt0"]["max_power_limit"]["value"];
+	    }
+	  } catch (error) {
+	    return 0.0;
+	  }
+	})
+      },
+      clocks: {
+        graphics: amdParseInt(gpu_data["clock"]["gfx_0"]["clk"]["value"]),
+        memory: amdParseInt(gpu_data["clock"]["mem_0"]["clk"]["value"]),
+      },
+      fan: {
+        speed: speed,
+      }
+    };
+  });
+
+  return gpus;
+}
+
+function amdParseFloat(value) {
+    try {
+        const ret = parseFloat(value);
+        return ret;
+    } catch(error) {
+        return 0.0;
+    }
+}
+
+function amdParseInt(value) {
+    try {
+        const ret = parseInt(value);
+        return ret;
+    } catch(error) {
+        return 0;
+    }
+}
